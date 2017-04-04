22d7faa9bf3bb46e90d5dd99b093905596fb6dc2
Maarten van Gompel <proycon@anaproy.nl>
2017-04-04 15:50:18 +0200

    making tests more portable, run PICCL scripts directly if possible

--- test.sh
+++ test.sh
@@ -5,43 +5,47 @@ if [ "$USER" == "travis" ]; then
    export PATH="/home/travis/build/LanguageMachines/PICCL:$PATH"
 fi
 
+PICCL="nextflow run LanguageMachine/PICCL"
 if [ -d /vagrant ] || [ ! -z "$VIRTUAL_ENV" ] || [ -f /usr/bin/TICCL-anahash ]; then
     #we are in LaMachine, no need for docker
     WITHDOCKER=""
+    PICCLDIR=$(dirname "${BASH_SOURCE[0]}")
+    if [ -f $PICCLDIR/ticcl.nf ]; then
+        PICCL=$PICCLDIR #run piccl scripts directly
+    fi
 else
     #we are not in LaMachine so use the docker LaMachine:
     WITHDOCKER="-with-docker proycon/lamachine"
 fi
 
-
 if [ ! -d data ]; then
     echo "Downloading data...">&2
     echo "----------------------------------------">&2
-    nextflow run LanguageMachines/PICCL/download-data.nf $WITHDOCKER || exit 2
+    $PICCL/download-data.nf $WITHDOCKER || exit 2
 fi
 
 if [ ! -d corpora ]; then
     echo "Downloading examples...">&2
     echo "----------------------------------------">&2
-    nextflow run LanguageMachines/PICCL/download-examples.nf $WITHDOCKER || exit 2
+    $PICCL/download-examples.nf $WITHDOCKER || exit 2
 fi
 
 echo "Testing OCR (eng) with inputtype pdfimages">&2
 echo "----------------------------------------">&2
-nextflow run LanguageMachines/PICCL/ocr.nf --inputdir corpora/PDF/ENG/ --language eng --inputtype pdfimages $WITHDOCKER || exit 2
+$PICCL/ocr.nf --inputdir corpora/PDF/ENG/ --language eng --inputtype pdfimages $WITHDOCKER || exit 2
 echo "Testing TICCL (eng)">&2
 echo "----------------------------------------">&2
-nextflow run LanguageMachines/PICCL/ticcl.nf --inputdir ocr_output/ --lexicon data/int/eng/eng.aspell.dict --alphabet data/int/eng/eng.aspell.dict.lc.chars --charconfus data/int/eng/eng.aspell.dict.c0.d2.confusion $WITHDOCKER || exit 2
+$PICCL/ticcl.nf --inputdir ocr_output/ --lexicon data/int/eng/eng.aspell.dict --alphabet data/int/eng/eng.aspell.dict.lc.chars --charconfus data/int/eng/eng.aspell.dict.c0.d2.confusion $WITHDOCKER || exit 2
 ls ticcl_output/*xml || exit 2
 
 rm -Rf ocr_output ticcl_output
 
 echo "Testing OCR (nld) with inputtype tif">&2
 echo "----------------------------------------">&2
-nextflow run LanguageMachines/PICCL/ocr.nf --inputdir corpora/TIFF/NLD/ --inputtype tif --language nld $WITHDOCKER || exit 2
+$PICCL/ocr.nf --inputdir corpora/TIFF/NLD/ --inputtype tif --language nld $WITHDOCKER || exit 2
 echo "Testing TICCL (nld)">&2
 echo "----------------------------------------">&2
-nextflow run LanguageMachines/PICCL/ticcl.nf --inputdir ocr_output/ --lexicon data/int/nld/nld.aspell.dict --alphabet data/int/nld/nld.aspell.dict.lc.chars --charconfus data/int/nld/nld.aspell.dict.c20.d2.confusion $WITHDOCKER || exit 2
+$PICCL/ticcl.nf --inputdir ocr_output/ --lexicon data/int/nld/nld.aspell.dict --alphabet data/int/nld/nld.aspell.dict.lc.chars --charconfus data/int/nld/nld.aspell.dict.c20.d2.confusion $WITHDOCKER || exit 2
 
 ls ticcl_output/*xml || exit 2
 
@@ -61,5 +65,5 @@ fi
 
 echo "Testing tokenisation pipeline from plain text ">&2
 echo "-----------------------------------------------">&2
-nextflow run LanguageMachines/PICCL/tokenize.nf --inputdir text_input --inputformat text --language nld $WITHDOCKER || exit 2
+$PICCL/tokenize.nf --inputdir text_input --inputformat text --language nld $WITHDOCKER || exit 2
 
