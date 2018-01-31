a4873d8d1e926096f0a49e61fc47848beeba1aa2
Maarten van Gompel <proycon@anaproy.nl>
2018-01-31 23:05:43 +0100

    better error message

--- webservice/picclservice/picclservice_wrapper.py
+++ webservice/picclservice/picclservice_wrapper.py
@@ -152,7 +152,8 @@ print(open('trace.txt','r',encoding='utf-8').read(), file=sys.stderr)
 
 if 'frog' in clamdata and clamdata['frog']:
     if lang != 'nld':
-        print("Input document is not dutch, defiantly ignoring linguistic enrichment choice!!!",file=sys.stderr)
+        print("Input document is not dutch (got + " + str(lang) + "), defiantly ignoring linguistic enrichment choice!!!",file=sys.stderr)
+        fail()
     else:
         clam.common.status.write(statusfile, "Running Frog Pipeline (linguistic enrichment)",75) # status update
         if os.system("nextflow run LanguageMachines/PICCL/frog.nf --inputdir " + shellsafe(ticcl_outputdir,'"') + " --extension folia.xml --outputdir " + shellsafe(outputdir,'"') + " -with-trace >&2"  ) != 0:
