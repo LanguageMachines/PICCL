7368a8b76874b1f2725914df353647eb024265b3
Maarten van Gompel <proycon@anaproy.nl>
2018-06-05 15:45:02 +0200

    dropping python 2 support

--- webservice/setup.py
+++ webservice/setup.py
@@ -23,7 +23,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         "Topic :: Text Processing :: Linguistic",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3", #3.0, 3.1 and 3.2 are not supported by flask
         "Programming Language :: Python :: 3.4", #3.0, 3.1 and 3.2 are not supported by flask
         "Programming Language :: Python :: 3.5", #3.0, 3.1 and 3.2 are not supported by flask
