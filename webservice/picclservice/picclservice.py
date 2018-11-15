df01df4d59b72cf0c6f9ae2b25c1c403f710e597
Maarten van Gompel <proycon@anaproy.nl>
2018-11-15 22:10:40 +0100

    syntax

--- webservice/picclservice/picclservice.py
+++ webservice/picclservice/picclservice.py
@@ -356,10 +356,10 @@ PARAMETERS = [
     ]),
     ('Tokenisation', [
         ChoiceParameter('ucto','Enable Tokenisation?',"Perform tokenisation using ucto? (works for various languages). There is no need to enable this if you also enable Frog below.", choices=[('yes','Yes'),('no','No')], default='yes'),
-    ])
+    ]),
     ('Linguistic Enrichment', [
         ChoiceParameter('frog','Enable Linguistic Enrichment?',"Perform linguistic enrichment using Frog? This works for dutch only. Use the next two categories to fine-tune your selection.", choices=[('yes','Yes'),('no','No')], default='yes'),
-    ])
+    ]),
     ('Basic enrichments steps (recommended)', [
         BooleanParameter('tok','Tokenisation',"Perform tokenisation", default=True),
         BooleanParameter('pos','Part-of-Speech Tagging',"Part-of-speech Tagging (for Dutch only!)",default=True),
