1bf16275f54f9be0eea05b22b005d8171167ef92
Maarten van Gompel <proycon@anaproy.nl>
2018-11-16 12:22:32 +0100

    updated startserver* scripts, LaMachine-aware

--- webservice/startserver_development.sh
+++ webservice/startserver_development.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ ! -z $LM_SOURCEPATH ]; then
+    export CONFIGFILE=$LM_SOURCEPATH/PICCL/webservice/picclservice/piccl.config.yml
+fi
+python3 setup.py develop
 clamservice -d picclservice.picclservice
